a534ef11831569a131bcb4310fe8204cc6455436
SharifMuhammadSifat <166907907+SharifMuhammadSifat@users.noreply.github.com>
2026-03-17 02:35:51 +0600

    login authentication functionality added

---
+++ backend.js
@@ -0,0 +1,23 @@
+const loginPage = document.getElementById("login-container");
+const username = document.getElementById("input-name");
+const password = document.getElementById("input-pass");
+const mainPage = document.getElementById("main-page-container");
+const submitBtn = document.getElementById("submit");
+
+
+
+mainPage.classList.add("hidden")
+
+
+
+
+const loginAuth = (u,p) => {
+    if ((u === "admin") && (p === "admin123")) {
+        loginPage.classList.add("hidden");
+        mainPage.classList.remove("hidden");
+    }
+}
+
+submitBtn.addEventListener("click", () => {
+    loginAuth(username.value, password.value)
+});
