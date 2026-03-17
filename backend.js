2bc9b1556fe636d1a0bb066b07a7f7d9ef878cb8
SharifMuhammadSifat <166907907+SharifMuhammadSifat@users.noreply.github.com>
2026-03-17 23:27:12 +0600

    tab toggling

--- backend.js
+++ backend.js
@@ -5,10 +5,13 @@ const mainPage = document.getElementById("main-page-container");
 const submitBtn = document.getElementById("submit");
 
 
+const allTabBtn = document.getElementById("all-btn");
+const openTabBtn = document.getElementById("open-btn");
+const closeTabBtn = document.getElementById("close-btn");
 
-// mainPage.classList.add("hidden")
 
 
+// mainPage.classList.add("hidden");
 
 
 const loginAuth = (u,p) => {
@@ -21,3 +24,79 @@ const loginAuth = (u,p) => {
 submitBtn.addEventListener("click", () => {
     loginAuth(username.value, password.value)
 });
+
+
+allTabBtn.addEventListener("click", () => {
+    const allTab = document.getElementById("all-tab");
+    const openTab = document.getElementById("open-tab");
+    const closeTab = document.getElementById("close-tab");
+
+        
+    allTabBtn.classList.add("btn-primary", "text-white");
+    allTabBtn.classList.remove("text-gray-500");
+
+    openTabBtn.classList.remove("text-white");
+    openTabBtn.classList.add("text-gray-500");
+    
+    closeTabBtn.classList.remove("text-white");
+    closeTabBtn.classList.add("text-gray-500");
+    
+    openTabBtn.classList.remove("btn-primary");
+    closeTabBtn.classList.remove("btn-primary");
+    allTab.classList.remove("hidden");
+    closeTab.classList.add("hidden");
+    openTab.classList.add("hidden");
+
+
+})
+
+openTabBtn.addEventListener("click", () => {
+    const allTab = document.getElementById("all-tab");
+    const openTab = document.getElementById("open-tab");
+    const closeTab = document.getElementById("close-tab");
+
+    openTabBtn.classList.add("btn-primary", "text-white");
+    openTabBtn.classList.remove("text-gray-500");
+    
+    allTabBtn.classList.remove("text-white");
+    allTabBtn.classList.add("text-gray-500");
+
+    closeTabBtn.classList.remove("text-white");
+    closeTabBtn.classList.add("text-gray-500")
+    allTabBtn.classList.remove("btn-primary");
+    closeTabBtn.classList.remove("btn-primary");
+    openTab.classList.remove("hidden");
+    closeTab.classList.add("hidden");
+    allTab.classList.add("hidden");
+    
+    
+
+})
+
+closeTabBtn.addEventListener("click", () => {
+    const allTab = document.getElementById("all-tab");
+    const openTab = document.getElementById("open-tab");
+    const closeTab = document.getElementById("close-tab");
+
+        closeTabBtn.classList.add("btn-primary", "text-white");
+        closeTabBtn.classList.remove("text-gray-500");
+
+        allTabBtn.classList.remove("text-white");
+        allTabBtn.classList.add("text-gray-500");
+
+        openTabBtn.classList.remove("text-white");
+        openTabBtn.classList.add("text-gray-500");
+        
+
+        openTabBtn.classList.remove("btn-primary");
+        allTabBtn.classList.remove("btn-primary");
+
+        closeTab.classList.remove("hidden");
+
+        allTab.classList.add("hidden");
+
+        openTab.classList.add("hidden");
+
+    
+
+})
