e7496421c4c470e414645ea3eff76cf3d523a54b
SharifMuhammadSifat <166907907+SharifMuhammadSifat@users.noreply.github.com>
2026-03-24 18:19:06 +0600

    basic card display for default all tab

--- backend.js
+++ backend.js
@@ -20,16 +20,65 @@ const tabs = [allTab, openTab, closeTab];
 
 mainPage.classList.add("hidden");
 
+const displayAllIssue = (arr) => {
+    allTab.innerHTML = "";
+    for (let issue of arr) {
+        let newIssue = document.createElement("div");
+        
+        if (issue.status === "open"){
+            newIssue.classList.add("shadow", "rounded-2xl", "border-t-2", "border-green-500", "overflow-hidden");
+        }
+        else {
+            newIssue.classList.add("shadow", "rounded-2xl", "border-t-2", "border-purple-500", "overflow-hidden");
+        }
+        newIssue.innerHTML = `
+            <div class="bg-white p-2.5">
+                <div class="flex justify-between items-center">
+                    <img src="${issue.status === "open" ? "assets/Open-Status.png" : "assets/Closed-Status.png"}" class="w-6 h-6" alt="">
+                    <div class="h-6 w-20 rounded-full flex items-center justify-center ${issue.priority === "high" ? "bg-[#FEECEC]" : issue.priority === "medium" ? "bg-[#faffb4]" : "bg-[#dadada]"}">
+                        <p class="${issue.priority === "high" ? "text-red-500" : issue.priority === "medium" ? "text-yellow-500" : "text-gray-500"} text-sm">${issue.priority}</p>
+                    </div>
+                </div>
+            
+                <div class="flex flex-col gap-2 mt-2"> 
+                    <p class="font-semibold text-[14px] text-[#1F2937]">${issue.title}</p>
+                    <p class="text-[#64748B] text-[12px]">${issue.description}</p>
+                </div>
+            
+                <div class="issue-labels mt-2">
+                    <p>${issue.labels.join(" ")}</p>
+                </div>
+            </div>
+            
+
+            <div class="border-t border-gray-300"></div>
+        
+            <div class="bg-white p-2.5">
+                <p class="text-[#64748B] text-[12px]">
+                    ${issue.author} <br>${issue.createdAt}
+                </p>
+            </div>
+        `;
+        allTab.append(newIssue);
+    }
+}
 
 const loginAuth = (u,p) => {
     if ((u === "admin") && (p === "admin123")) {
         loginPage.classList.add("hidden");
         mainPage.classList.remove("hidden");
+        fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
+        .then((res) => res.json())
+        .then((issue_obj) => displayAllIssue(issue_obj.data));
     }
 }
 
-submitBtn.addEventListener("click", () => {
+
+
+submitBtn.addEventListener("click", (event) => {
+    // event.preventDefault();
     loginAuth(username.value, password.value)
+    
 });
 
 
@@ -85,6 +134,12 @@ function switchTab(activeBtn, activeTab) {
     activeTab.classList.remove("hidden");
 }
 
-allBtn.addEventListener("click", () => switchTab(allBtn, allTab));
-openBtn.addEventListener("click", () => switchTab(openBtn, openTab));
-closeBtn.addEventListener("click", () => switchTab(closeBtn, closeTab));
+allBtn.addEventListener("click", () => {
+    switchTab(allBtn, allTab)
+
+});
+
+
+
+openBtn.addEventListener("click", () => {switchTab(openBtn, openTab)});
+closeBtn.addEventListener("click", () => {switchTab(closeBtn, closeTab)});
