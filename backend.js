f1fe408ffdeff7a5097e44337be1ff9c79dda718
SharifMuhammadSifat <166907907+SharifMuhammadSifat@users.noreply.github.com>
2026-03-24 19:52:32 +0600

    badgeHandler final fix

--- backend.js
+++ backend.js
@@ -24,31 +24,31 @@ const badgeHandler = (arr) => {
     let badges = ""
     for (let badge of arr){
         if (badge === "bug"){
-            badges += `<span class="px-3 py-1 text-sm rounded-full bg-red-100 text-[#db1616ff]">
+            badges += `<span class="px-3 py-1 text-[12px] rounded-full bg-red-100 text-[#db1616ff]">
                                 <i class="fa-solid fa-bug" style="color: #db1616;"></i> BUG
                         </span> `;
         }
 
         else if (badge === "help wanted"){
-            badges += `<span class="px-3 py-1 text-sm rounded-full bg-orange-100 text-[#db7900ff]">
+            badges += `<span class="px-3 py-1 text-[12px] rounded-full bg-orange-100 text-[#db7900ff]">
                             <i class="fa-solid fa-life-ring" style="color: rgb(219, 122, 0);"></i> HELP WANTED
                         </span> `;
         }
 
         else if (badge === "good first issue"){
-            badges += `<span class="px-2.5 py-0.5 text-xs font-semibold rounded-full bg-purple-100 text-[#ac56bfff]">
+            badges += `<span class="px-2.5 py-0.5 text-[12px] font-semibold rounded-full bg-purple-100 text-[#ac56bfff]">
                            <i class="fa-solid fa-circle-exclamation" style="color: #ac56bf;"></i> GOOD FIRST ISSUE
                         </span>`;
         }
 
         else if (badge === "documentation"){
-            badges += `<span class="px-2.5 py-0.5 text-xs font-semibold rounded-full bg-blue-100 text-[#5689bf]">
+            badges += `<span class="px-2.5 py-0.5 text-[12px] font-semibold rounded-full bg-blue-100 text-[#5689bf]">
                         <i class="fa-brands fa-readme" style="color: #5689bf;"></i>  DOCUMENTATION
                     </span>`
         }
 
         else{
-            badges += `<span class="px-3 py-1 text-sm rounded-full bg-green-100 text-[#1adb00ff]">
+            badges += `<span class="px-3 py-1 text-[12px] rounded-full bg-green-100 text-[#1adb00ff]">
                             <i class="fa-regular fa-star" style="color: #1adb00;"></i> ENHANCEMENT
                         </span> `;
         }
@@ -84,7 +84,7 @@ const displayAllIssue = (arr) => {
                 </div>
             
                 <div class="issue-labels mt-2">
-                    <p>${badgeHandler(issue.labels)}</p>
+                    <div class="flex flex-col gap-1 2xl:flex-row ">${badgeHandler(issue.labels)}</div>
                 </div>
             </div>
             
@@ -120,39 +120,6 @@ submitBtn.addEventListener("click", (event) => {
 });
 
 
-//<div class="shadow rounded-2xl  border-t-2 border-green-500 overflow-hidden">
-//
-//                            <!-- Top Section -->
-//                            <div class="bg-white p-2.5">
-//                                <div class="flex justify-between items-center">
-//                                    <img src="assets/Open-Status.png" class="w-6 h-6" alt="">
-//
-//                                    <div class="h-6 w-20 bg-[#FEECEC] rounded-full flex items-center justify-center">
-//                                        <p class="text-[#EF4444] text-sm">HIGH</p>
-//                                    </div>
-//                                </div>
-//                            
-//                                <div class="flex flex-col gap-2 mt-2"> 
-//                                    <p class="font-semibold text-[14px] text-[#1F2937]">demo text</p>
-//                                    <p class="text-[#64748B] text-[12px]">demo text</p>
-//                                </div>
-//                            
-//                                <div class="issue-labels mt-2">
-//                                    <!-- labels here -->
-//                                </div>
-//                            </div>
-//                            
-//                            <!-- Divider -->
-//                            <div class="border-t border-gray-300"></div>
-//                        
-//                            <!-- Bottom Section -->
-//                            <div class="bg-white p-2.5">
-//                                <p class="text-[#64748B] text-[12px]">
-//                                    programmer <br>date
-//                                </p>
-//                            </div>
-//
-//                        </div>
 
 
 function switchTab(activeBtn, activeTab) {
